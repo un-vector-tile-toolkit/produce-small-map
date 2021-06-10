8044ebcdf4a26604c2e5bb4ed85411b9f58aa1d9
ubukawa <ubukawa-t96dq@mlit.go.jp>
2021-06-09 23:46:55 -0400

    Update work_small.sh

--- work_small.sh
+++ work_small.sh
@@ -1,3 +1,3 @@
 node produce-small/index.js
 /usr/local/bin/tile-join --no-tile-size-limit -e small_tiles/smalltile small_tiles/0-0-0.mbtiles --no-tile-compression
-scp -i XXX(path to your ssh key) -r ./small_tiles/unosm/small-scale.mbtiles (username)@(hostingserver):(path)/public/small-map-test
+
